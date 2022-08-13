8a5ee12a74c126e3f167a76a8ad36fa4ff03fd8a
Karar Haider <karar3k@yahoo.com>
2022-08-13 23:23:13 +0300

    Python

--- logic-pass/Q3.py
+++ logic-pass/Q3.py
@@ -1,10 +1,10 @@
 # Q3/write a function that count how many the given character repeated in a given string?
 
-def my_function(original_string, character):
+def countChar(original_string, character):
     repute_number  = 0
     for a in original_string:
         if a == character:
             repute_number += 1
     print(repute_number)
 
-my_function("Hello World","H")
+countChar("Hello World","H")
