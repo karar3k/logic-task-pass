04739530f812f5cc22c93d90a85045a830e7df19
karar3k <31390170+karar3k@users.noreply.github.com>
2022-10-12 09:47:20 +0300

    Python Q2

--- logic-pass/Q2.py
+++ logic-pass/Q2.py
@@ -1,11 +1,9 @@
-# Q2/Write a program to find all prime numbers up to a given range of numbers?
+def countChar(original_string, character):
+    repute_number  = 0
+    for char in original_string:
+        if char == character:
+            repute_number += 1
+    print(repute_number)
 
-def is_prime(lower_value ,upper_value ):
-    for number in range(lower_value,upper_value + 1):
-        if number > 1:
-            for i in range (2, number):  
-                if (number % i) == 0:
-                    break
-            else:
-                print("Is Prime : ", number)
-is_prime(2,15)
+countChar("Hello World","H")
+
